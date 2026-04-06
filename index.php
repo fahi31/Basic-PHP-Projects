a812b0ade4bebf2ff20279c0f7908f704a943884
Maruful Hasan Fahim <fahimahmed30435@gmail.com>
2026-04-06 08:10:57 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$age = 10;
+if($age >= 0 && $age <= 12 )
+    {
+        echo "Child";
+    }
+elseif($age >= 13 && $age <= 19)
+    {
+        echo "Teenager";
+    }
+elseif($age >= 20)
+{
+    echo "Adult";
+}
+else
+    {
+        echo "No";
+    }
+
+
+
+
+
+?>
